1c958f5e96ca9f8a339bd85417ee486d6359b5d1
Effes77 <alexandervl928@gmail.com>
2024-12-18 17:13:32 +0200

    ok

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -1,20 +1,20 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-container', {
     loop: true,
-    lidesPerView: 1,
+    slidesPerView: 1,
     spaceBetween: 20,
     breakpoints: {
-    480: {
-      slidesPerView: 2,
-    },
-    768: {
+      480: {
+        slidesPerView: 2,
+      },
+      768: {
       slidesPerView: 3,
-    }
+      }
   },
     pagination: {
-    el: '.swiper-pagination',
+      el: '.swiper-pagination',
     },
     navigation: {
-    nextEl: '.carousel-button.next',
-    prevEl: '.carousel-button.prev',
+      nextEl: '.carousel-button.next',
+      prevEl: '.carousel-button.prev',
   },
 });
